0cc5bfa2c0d676bc19da54e97375955a3c29b479
gumaper <gumaper@outlook.com.br>
2022-12-07 19:18:13 -0400

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,115 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { FormBuilder, FormControl } from '@angular/forms';
+import html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'teste';
+export class AppComponent implements OnInit {
+  title = 'lamina-paula';
+
+  bgImage: string = '';
+
+  cidade: FormControl = new FormControl();
+  subTitulo: FormControl = new FormControl();
+  data: FormControl = new FormControl();
+
+  valor: FormControl = new FormControl();
+
+  textBox: FormControl = new FormControl();
+
+  headerFontSize: string = '';
+  fontSizeSubTitulo: string = '';
+  fontSizeData: string = '';
+
+  constructor(private _formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    let value =
+      '7 NOITES DE HOSPEDAGEM COM CAFÉ DA MANHÃ\nPREÇO POR PESSOA EM APARTAMENTO DUPLO\nTAXAS INCLUSAS\nSUJEIRO A ALTERAÇÃO DE PREÇO SEM AVISO PRÉVIO\nPASSAGEM AÉREA IDA E VOLTA\nVIAGEM NACIONAL\nTRANSLADO DO AEROPORTO P/HOTEL\nVISITA Á CIDADE E CUMBUCO';
+
+    this.textBox.setValue(value);
+  }
+
+  abrirFiles() {
+    document.getElementById('fileFeed')?.click();
+  }
+
+  onCidade() {
+    console.log(this.cidade.value.length);
+
+    if (this.cidade.value.length < 10) {
+      this.headerFontSize = '70px';
+    }
+
+    if (this.cidade.value.length > 10) {
+      this.headerFontSize = '61px';
+    }
+
+    if (this.cidade.value.length >= 15) {
+      this.headerFontSize = '50px';
+    }
+
+    if (this.cidade.value.length >= 20) {
+      this.headerFontSize = '42px';
+    }
+
+    if (this.cidade.value.length >= 25) {
+      this.headerFontSize = '34px';
+    }
+  }
+
+  onSubTitulo() {
+    if (this.subTitulo.value.length > 20) {
+      this.fontSizeSubTitulo = '26px';
+    } else {
+      this.fontSizeSubTitulo = '42px';
+    }
+  }
+
+  onData() {
+    let value = this.data.value.length;
+    console.log(value);
+
+    if (value < 16) {
+      this.fontSizeData = '64px';
+    }
+
+    if (value > 16) {
+      this.fontSizeData = '51px';
+    }
+
+    if (value > 20) {
+      this.fontSizeData = '42px';
+    }
+
+    if (value > 24) {
+      this.fontSizeData = '32px';
+    }
+
+    if (value > 30) {
+      this.fontSizeData = '24px';
+    }
+  }
+
+  setarBackground(event: any) {
+    this.bgImage = URL.createObjectURL(event.files[0]);
+
+    console.log(this.bgImage);
+  }
+
+  @ViewChild('canvas') canvas: any;
+  @ViewChild('downloadLink') downloadLink: any;
+  @ViewChild('screen') screen: any;
+
+  imprimirStories() {
+    html2canvas(this.screen.nativeElement).then((canvas) => {
+      this.canvas.nativeElement.src = canvas.toDataURL();
+      this.downloadLink.nativeElement.href = canvas.toDataURL('image/png');
+      this.downloadLink.nativeElement.download = 'marble-diagram.png';
+      this.downloadLink.nativeElement.click();
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatCheckboxModule,
+    MatInputModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
